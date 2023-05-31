5406163a3d2bbea73e9358dae8effa9dbb14bc91
Julia Georgiew <java2011@me.com>
2023-05-31 21:18:35 +0200

    card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+
 
 @Component({
   selector: 'app-game',
@@ -7,13 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string;
+  game !: Game;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
 
   }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+
   takeCard() {
+
+    if(!this.pickCardAnimation){
+    this.currentCard = this.game.stack.pop();
+   
     this.pickCardAnimation = true;
+
+    setTimeout(()=> {
+      this.game.playedCards.push(this.currentCard);
+      this.pickCardAnimation = false;
+    },1000)
+  }
   }
 
 }
